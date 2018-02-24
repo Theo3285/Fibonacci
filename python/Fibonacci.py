b1256588a9710064464efab4625470f5acf3a94f
Christophe THEPAUT <christophe@thepaut.net>
2018-02-24 14:16:27 +0100

    add doctests

--- python/Fibonacci.py
+++ python/Fibonacci.py
@@ -1,5 +1,27 @@
 class Fibonacci(object):
     def generate(self, length):
+        """Generate a Fibonacci list
+
+        :param length:
+            the length of the list to generate
+        :return:
+            an array list of numbers
+
+        >>> f = Fibonacci()
+        >>> f.generate(1)
+        [0]
+        >>> f.generate(2)
+        [0, 1]
+        >>> f.generate(3)
+        [0, 1, 1]
+        >>> f.generate(4)
+        [0, 1, 1, 2]
+        >>> f.generate(5)
+        [0, 1, 1, 2, 3]
+        >>> f.generate(10)
+        [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+        """
         sequence = []
         for index in range(length):
             if index < 2:
